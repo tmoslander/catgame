93929b274d9f55d1135431e17372ab0f8090ee91
gjf2a <ferrer@hendrix.edu>
2021-02-05 12:36:32 -0600

    Updated to clear the screen at startup.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use spin::Mutex;
 use pc_keyboard::DecodedKey;
 use pluggable_interrupt_template::LetterMover;
 use pluggable_interrupt_os::HandlerTable;
+use pluggable_interrupt_os::vga_buffer::clear_screen;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     HandlerTable::new()
         .keyboard(key)
         .timer(tick)
+        .startup(clear_screen)
         .start()
 }
 
