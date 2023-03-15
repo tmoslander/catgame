95f8dba426f61d7d05e1a66d4dbd687466999463
tmoslander <bmoslander0624@gmail.com>
2023-03-14 19:25:16 -0500

    More catcore stuff (finished?)

--- src/cat_core.rs
+++ src/cat_core.rs
@@ -79,12 +79,37 @@ pub struct Position<const WIDTH: usize, const HEIGHT: usize>{
     col: i16, row: i16
 }
 
+impl <const WIDTH: usize, const HEIGHT: usize> Add for Position<WIDTH,HEIGHT> {
+    type Output = Position<WIDTH,HEIGHT>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Position {col: self.col + rhs.col, row: self.row + rhs.row}
+    }
+}
+
+impl <const WIDTH: usize, const HEIGHT: usize> Sub for Position<WIDTH,HEIGHT> {
+    type Output = Position<WIDTH,HEIGHT>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Position {col: self.col - rhs.col, row: self.row - rhs.row}
+    }
+}
+
+
 impl <const WIDTH: usize, const HEIGHT: usize> Position<WIDTH, HEIGHT>{
     pub fn is_legal(&self) -> bool{
         0<= self.col && self.col < WIDTH as i16 && 0<= self.row && self.row < HEIGHT as i16
     }
     pub fn row_col(&self) -> (usize, usize){
         (self.row as usize, self.col as usize)
+    }
+    pub fn neighbor(&self, d: Dir) -> Position<WIDTH,HEIGHT> {
+        match d {
+            Dir::N => Position {row: self.row - 1, col: self.col},
+            Dir::S => Position {row: self.row + 1, col: self.col},
+            Dir::E => Position {row: self.row,     col: self.col + 1},
+            Dir::W => Position {row: self.row,     col: self.col - 1}
+        }
     }
 }
 
@@ -166,7 +191,7 @@ pub enum Status{
 
 
 
-impl<const WIDTH:usize, const HEIGHT: usize> catgame<WIDTH, HEIGHT>{
+impl<const WIDTH:usize, const HEIGHT: usize> CatGame<WIDTH, HEIGHT>{
     pub fn new() -> Self{
         let mut game = CatGame{
             cat : Cat::new(Position {col: 0, row: 0}),
@@ -207,52 +232,169 @@ impl<const WIDTH:usize, const HEIGHT: usize> catgame<WIDTH, HEIGHT>{
     pub fn cell_pos_iter(&self) -> RowColIter<WIDTH, HEIGHT>{
         RowColIter{row:0, col:0}
     }
-
-
-
-
-pub fn cat_at(&self) -> Position<WIDTH, HEIGHT>{
+    pub fn cat_at(&self) -> Position<WIDTH, HEIGHT>{
     self.cat.pos
-}
-
-pub fn dog_at(&self) -> Postion<WIDTH,HEIGHT>{
-    self.dogs.iter().enumerate().find(|(_,dog)| dog.pos == p)
-}
-
-pub fn cat_icon(&self) -> char{
-    self.cat.icon()
-}
-
-
-pub fn update(&mut self){
-    self.last_key = None;
-    self.cat.tick();
-    self.empower_tick();
-    self.update_dogs();
-}
-
-
-
-fn update_dogs(&mut self){
-    for d in 0..self..dogs.len(){
-        // make it so that the dogs randomly wander (Ferrer question)
-        self.resolve_dog_col(d);
-        }
-}
-
-fn resolve_dog_col(&mut self, d:usize){
-    if self.dogs[d].pos == self.cat.pos && self.dogs[d].active{
-        match self.status{
-            Status::Normal => self.status = Status.Over,
-            Status::Empowered => {
-                self.fish_eaten += 100;
-                self.dogs[d].squash();
+    }
+    pub fn cat_icon(&self) -> char{
+        self.cat.icon()
+    }
+    pub fn dog_at(&self) -> Postion<WIDTH,HEIGHT>{
+        self.dogs.iter().enumerate().find(|(_,dog)| dog.pos == p)
+    }
+    pub fn update(&mut self){
+        self.last_key = None;
+        self.cat.tick();
+        self.empower_tick();
+        self.update_dogs();
+    }
+    fn update_dogs(&mut self){
+        for d in 0..self.dogs.len() {
+            let (ahead, left, right) = self.ahead_left_right(self.dogs[d].pos, self.dogs[d].dir);
+            self.resolve_dog_collision(d);
+            self.dogs[d].go(ahead, left, right, self.cat_pos);
+            self.resolve_dog_collision(d);
+            // make it so that the dogs randomly wander (Ferrer question)
+            self.resolve_dog_col(d);
             }
-            Status::Over => {}
+        }
+    fn resolve_dog_collision(&mut self, d:usize){
+        if self.dogs[d].pos == self.cat.pos && self.dogs[d].active{
+            match self.status{
+                Status::Normal => self.status = Status.Over,
+                Status::Over => {}
+            }
+        }
+    }
+    fn ahead_left_right(&self, p: Position<WIDTH,HEIGHT>, dir: Dir) -> (Cell,Cell,Cell) {
+        let ahead = self.cell(p.neighbor(dir));
+        let left = self.cell(p.neighbor(dir.left()));
+        let right = self.cell(p.neighbor(dir.right()));
+        (ahead, left, right)
+    }
+
+    pub fn countdown_complete(&mut self) -> bool {
+        if self.countdown == 0 {
+            self.countdown = UPDATE_FREQUENCY;
+            true
+        } else {
+            self.countdown -= 1;
+            false
+        }
+    }
+
+    pub fn key(&mut self, key: DecodedKey) {
+        match self.status {
+            Status::Over => {
+                match key {
+                    DecodedKey::RawKey(KeyCode::S) | DecodedKey::Unicode('s') => self.reset(),
+                    _ => {}
+                }
+            }
+            _ => {
+                let key = key2dir(key);
+                if key.is_some() {
+                    self.last_key = key;
+                }
+            }
+        }
+    }
+
+    fn resolve_move(&mut self) {
+        if let Some(dir) = self.last_key {
+            let neighbor = self.cat.pos.neighbor(dir);
+            if neighbor.is_legal() {
+                let (row, col) = neighbor.row_col();
+                if self.cells[row][col] != Cell::Wall {
+                    self.move_to(neighbor, dir);
+                }
+            }
+        }
+    }
+
+    fn move_to(&mut self, neighbor: Position<WIDTH,HEIGHT>, dir: Dir) {
+        self.cat.pos = neighbor;
+        self.cat.dir = dir;
+        let (row, col) = neighbor.row_col();
+        match self.cells[row][col] {
+            Cell::Fish => {
+                self.fish_eaten += 1;
+                self.cells[row][col] = Cell::Empty;
+            }
+        }
+    }
+
+    pub fn status(&self) -> Status {
+        self.status
+    }
+}
+//end of super long catgame impl
+
+fn key2dir(key: DecodedKey) -> Option<Dir> {
+    match key {
+        DecodedKey::RawKey(k) => match k {
+            KeyCode::ArrowUp => Some(Dir::N),
+            KeyCode::ArrowDown => Some(Dir::S),
+            KeyCode::ArrowLeft => Some(Dir::W),
+            KeyCode::ArrowRight => Some(Dir::E),
+            _ => None
+        }
+        DecodedKey::Unicode(c) => match c {
+            'w' => Some(Dir::N),
+            'a' => Some(Dir::W),
+            's' => Some(Dir::S),
+            'd' => Some(Dir::E),
+            _ => None
         }
     }
 }
 
+pub struct RowColIter<const WIDTH: usize, const HEIGHT: usize> {
+    row: usize, col: usize
 }
+
+impl <const WIDTH: usize, const HEIGHT: usize> Iterator for RowColIter<WIDTH,HEIGHT> {
+    type Item = Position<WIDTH,HEIGHT>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.row < HEIGHT {
+            let result = Some(Position {row: self.row as i16, col: self.col as i16});
+            self.col += 1;
+            if self.col == WIDTH {
+                self.col = 0;
+                self.row += 1;
+            }
+            result
+        } else {
+            None
+        }
+    }
+}
+
+const DOG_START_DIR: [Dir; 2] = [Dir::E, Dir::W];
+
+const START: &'static str =
+    "################################################################################
+     #.........D....................................................................#
+     #.#################.##.##.###.####.#.##############.##.##.##.##.################
+     #.#################.##.##.###.####.#.##############.##.##.##.##.################
+     #.#################.##.##.###.####.#.##############.##.##.##.##.################
+     #..............................................................................#
+     ###.####.#####.######.####.#.#.#######.#.####.####.#.######.#.####.###.###.##.##
+     ###.####.#####.######.####.#.#.#######.#.####.####.#.######.#.####.###.###.##.##
+     ###.####.#####.######.####.#.#.#######.#.####.####.#.######.#.####.###.###.##.##
+     ###.####.#####.######.####.#.#.#######.#.####.####.#.######.#.####.###.###.##.##
+     #......................................C.......................................#
+     #####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.##
+     #####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.##
+     #####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.##
+     #####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.##
+     #####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.##
+     #####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.#####.##
+     #....................................................................f.........#
+     ####.####.####.####.####.####.####.####.####.####.####.####.####.####.####.##.##
+     ####.####.####.####.####.####.####.####.####.####.####.####.####.####.####.##.##
+     ####.####.####.####.####.####.####.####.####.####.####.####.####.####.####.##.##
+     #......................................................................D.......#
+     ################################################################################";
 
 // need to match keys to directions being moved, maybe need dir after all without the icon matching.
